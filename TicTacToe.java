c8e7ae0259efca388e3f5a093e3b9a9bbf6704a5
Arnim Taliyan <arnimchaudhary@gmail.com>
2025-07-21 11:13:02 +0530

    Check Winner Method

---
+++ TicTacToe.java
@@ -0,0 +1,45 @@
+package com.gfg.Project;
+
+import java.util.Arrays;
+
+public class TicTacToe {
+    static String[] board;
+    static String turn;
+
+    public static String checkWinner() {
+        for (int i = 0; i < 8; i++) {
+            String line = null;
+
+            switch(i) {
+                case 0 -> board[0] + board[1] + board[2];
+                case 1 -> board[3] + board[4] + board[5];
+                case 2 -> board[6] + board[7] + board[8];
+                case 3 -> board[0] + board[3] + board[6];
+                case 4 -> board[1] + board[4] + board[7];
+                case 5 -> board[2] + board[5] + board[8];
+                case 6 -> board[0] + board[4] + board[8];
+                case 7 -> board[2] + board[4] + board[6];
+            }
+
+            if (line.equals("XXX")) {
+                return "X";
+            } else if (line.equals("OOO")) {
+                return "O";
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if(Arrays.asList(board).contains(String.valueOf(i + 1))) {
+                break;
+            } else if (i == 8) {
+                return "draw";
+            }
+        }
+
+        System.out.println(turn + "'s turn; enter a slot number to place " + turn + " in:");
+        return null;
+    }
+
+    public static void main(String[] args) {
+    }
+}
